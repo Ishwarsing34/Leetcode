3d5e640775c072e0a66901311a035a23057634eb
Ishwarsing34 <igirase65@gmail.com>
2024-09-05 19:17:07 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (73.69%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     string sortSentence(string s) {
-        string word;
+        int n = s.size();
         vector<string>v(10);
-        for(int i=0;i<s.size();i++) {
-            
-            if(s[i]>=48 && s[i]<=57)
+        string word="";
+        string ans="";
+
+        for(int i=0;i<n;i++){
+           
+            if(isdigit(s[i]))
             {
-                v[s[i]-48] = word + " ";
-                word = "";
-                i++;
+               v[s[i]-'0'] = word + " ";
+               word="";
+               i++;
             }
-            else word += s[i];
+            else word+=s[i];
         }
-        string ans;
-        for(string x:v)
-        {
-            ans+=x;
+
+        for(auto x:v){
+           ans+=x;
         }
         ans.pop_back();
         return ans;
