5945e56e7255cb7b0539375f68ffa82be5651562
Ishwarsing34 <igirase65@gmail.com>
2024-12-14 21:56:07 +0530

    Time: 15 ms (20.43%), Space: 10.9 MB (16.81%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,28 +2,20 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        
+        int right = 0, left = 0, ans = 0;
+        int hash[26] = {};
 
-        int right = 0;
-        int left = 0;
-        int maxLen = 0;
-        int cnt[26] = {};
-        int ans = 0;
+        for (right = 0; right < n; right++) {
+            hash[s[right] - 'A']++;
 
-        for(right=0;right<n;right++) {
-            cnt[s[right]-'A']++;
-
-            while(right-left+1 - *max_element(cnt,cnt+26) > k){
-                cnt[s[left]-'A']--;
+            while (right - left + 1 - *max_element(hash, hash + 26) > k) {
+                hash[s[left] - 'A']--;
                 left++;
             }
 
-            ans = max(ans,right-left+1);
+            ans = max(right - left + 1, ans);
         }
 
         return ans;
-
-
-
     }
 };
