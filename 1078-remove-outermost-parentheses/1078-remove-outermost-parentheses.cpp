e0594d7103aeb70ece151c191b574307fa4e4d14
Ishwarsing34 <igirase65@gmail.com>
2024-12-16 14:09:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.05%) - LeetHub

---
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        int n = s.size();
+        int cnt = 0;
+        string res = "";
+
+        for (int i = 0; i < n; i++) {
+
+            if (s[i] == '(') {
+                cnt++;
+                if (cnt > 1) {
+                    res.push_back(s[i]);
+                }
+            } else {
+                cnt--;
+                if (cnt > 0) {
+                    res.push_back(s[i]);
+                }
+            }
+        }
+
+        return res;
+    }
+};
