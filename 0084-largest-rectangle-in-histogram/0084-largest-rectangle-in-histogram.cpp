12831ce77b2d2d68c13de0e403154b5196c458b3
Ishwarsing34 <igirase65@gmail.com>
2024-12-27 15:58:22 +0530

    Time: 24 ms (69.53%), Space: 85.6 MB (35.64%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,39 +1,48 @@
-
-
 class Solution {
 public:
     int largestRectangleArea(vector<int>& h) {
         int n = h.size();
-        vector<int> prevsmall(n, -1); // Replace VLAs with vectors
-        vector<int> nextsmall(n, n); // Replace VLAs with vectors
+
+        vector<int> prev(n, -1);
+        vector<int> nxt(n, n);
 
         stack<int> st;
 
-        // Find previous smaller elements
         for (int i = 0; i < n; i++) {
             while (!st.empty() && h[st.top()] >= h[i]) {
                 st.pop();
             }
-            prevsmall[i] = st.empty() ? -1 : st.top();
+
+            prev[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
 
-        stack<int> nxt;
+        // stack<int>nxt;
 
-        // Find next smaller elements
-        for (int i = n - 1; i >= 0; i--) {
-            while (!nxt.empty() && h[nxt.top()] >= h[i]) {
-                nxt.pop();
-            }
-            nextsmall[i] = nxt.empty() ? n : nxt.top();
-            nxt.push(i);
+        while(!st.empty())
+        {
+            st.pop();
         }
 
-        // Calculate the maximum area
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && h[st.top()] >= h[i])
+            {
+                st.pop();
+            }
+
+            nxt[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+
         int area = 0;
-        for (int i = 0; i < n; i++) {
-            int width = nextsmall[i] - prevsmall[i] - 1;
-            area = max(area, width * h[i]);
+
+        for(int i=0;i<n;i++) 
+        {
+            int width = (nxt[i] - prev[i] - 1) * h[i];
+            area = max(width,area);
         }
 
         return area;
