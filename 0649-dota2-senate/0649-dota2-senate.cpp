b2daff0c264e1efa270ecdccabf5e042b699662d
Ishwarsing34 <igirase65@gmail.com>
2024-12-30 19:28:41 +0530

    Time: 47 ms (8.39%), Space: 8.9 MB (90.7%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    bool removeSenator(string &senate, char ch, int &idx) {
+        int n = senate.size();
+        for (int i = 0; i < n; ++i) {
+            int pos = (idx + i) % n;
+            if (senate[pos] == ch) {
+                senate.erase(begin(senate) + pos);
+                if (pos < idx) {
+                    idx--;
+                }
+                return pos < idx;
+            }
+        }
+        return false;
+    }
+
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.size();
+
+        int R_cnt = count(begin(senate), end(senate), 'R');
+        int D_cnt = n - R_cnt;
+
+        int idx = 0;
+
+        while (R_cnt > 0 && D_cnt > 0) {
+            if (senate[idx] == 'R') {
+                removeSenator(senate, 'D', idx);
+                D_cnt--;
+            } else {
+                removeSenator(senate, 'R', idx);
+                R_cnt--;
+            }
+
+            idx = (idx + 1) % senate.size();
+        }
+
+        return R_cnt == 0 ? "Dire" : "Radiant";
+    }
+};
