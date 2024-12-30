51c3bab0180e292a95485966e8273b553d891374
Ishwarsing34 <igirase65@gmail.com>
2024-12-30 19:47:06 +0530

    Time: 43 ms (8.66%), Space: 8.9 MB (91.91%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    bool RemoveSenate(string senate, char ch, int idx) {
-         int n = senate.size();
-        for (int i = 0; i < n; i++) {
-             int pos = (idx + i) % n;
+    bool removeSenator(string &senate, char ch, int &idx) {
+        int n = senate.size();
+        for (int i = 0; i < n; ++i) {
+            int pos = (idx + i) % n;
             if (senate[pos] == ch) {
-               
                 senate.erase(begin(senate) + pos);
                 if (pos < idx) {
                     idx--;
                 }
-
                 return pos < idx;
             }
         }
-
         return false;
     }
 
@@ -28,15 +25,14 @@ public:
 
         while (R_cnt > 0 && D_cnt > 0) {
             if (senate[idx] == 'R') {
-                RemoveSenate(senate, 'D', idx);
+                removeSenator(senate, 'D', idx);
                 D_cnt--;
             } else {
-
-                RemoveSenate(senate, 'R', idx);
-                 R_cnt--;
+                removeSenator(senate, 'R', idx);
+                R_cnt--;
             }
 
-            idx = (idx + 1)%senate.size();
+            idx = (idx + 1) % senate.size();
         }
 
         return R_cnt == 0 ? "Dire" : "Radiant";
