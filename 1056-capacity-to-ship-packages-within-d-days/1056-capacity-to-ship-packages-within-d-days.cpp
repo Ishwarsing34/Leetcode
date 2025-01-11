97e6bea511b3ed4dc2346a2cf4544df332ab46e6
Ishwarsing34 <igirase65@gmail.com>
2025-01-11 11:38:41 +0530

    Time: 8 ms (85.66%), Space: 34.9 MB (54.6%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -1,41 +1,45 @@
 class Solution {
-    bool  isPossible(int mid, vector<int>&weights, int days){
-        int cnt = 1, sum = 0;
+    bool isPossible(vector<int>&weights, int days, int mid){
 
-        for(int i = 0; i < weights.size(); i++){
+       int cnt = 1,sum = 0;
+
+       for(int i=0;i<weights.size();i++){
             sum+=weights[i];
+
             if(sum>mid){
                 cnt++;
                 sum = weights[i];
             }
-        }
+       }
 
-        if(cnt<=days){
-            return true;
-        }
+       if(cnt <= days) return true;
+    
        return false;
-        
     }
 public:
     int shipWithinDays(vector<int>& weights, int days) {
         int maxCap = 0;
+
         int n = weights.size();
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++)
+        {
             maxCap += weights[i];
         }
 
-        int low = *max_element(weights.begin(),weights.end()), high  = maxCap;
+        int low = *max_element(weights.begin(),weights.end());
+        int high = maxCap;
         int result = 0;
 
-        while(low<=high) {
 
-            int mid = low + (high - low)/2;
+        while(low<=high){
+             
+            int mid = low + (high - low ) / 2;
 
-            if(isPossible(mid, weights, days)) {
-                result =  mid;
+            if(isPossible(weights,days,mid)){
+                result = mid;
                 high = mid - 1;
-            } else{
+            }else{
                 low = mid + 1;
             }
         }
