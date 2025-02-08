f707c2a736ead85166e75b31769f7ded6bbc7a06
Ishwarsing34 <igirase65@gmail.com>
2025-02-08 15:40:47 +0530

    Time: 0 ms (100%), Space: 7.7 MB (99.82%) - LeetHub

--- 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/1951-find-the-winner-of-the-circular-game.cpp
@@ -1,35 +1,14 @@
 class Solution {
-    int winner(vector<bool>&person, int n, int k, int person_left, int ind)
-    {//brute 0 n^2
-        if(person_left == 1) {
-            for(int i=0;i<n;i++){
-                if(person[i] == 0) return i+1;
-            }
-        }
+    int winner(int n, int k)
+    {
+       if( n == 1) return 0;
 
-        int kill = (k-1)%person_left;//person_left ne div kela bcz n ne nhi kru shkt jar kela tr index wrong milnar
-         while(kill--){
-            ind = (ind + 1)%n;
-            while(person[ind] == 1){
+       return (winner(n - 1, k) + k) % n;
 
-                ind = (ind+1)%n;
-            
-            }
-         }
-
-         person[ind] = 1;
-
-         while(person[ind] == 1){
-             ind = (ind + 1)%n;
-         }
-
-        return winner(person,n,k,person_left-1,ind);
     }
 public:
     int findTheWinner(int n, int k) {
-        vector<bool>person(n,0);
-        int person_left = n;
-        int ind = 0;
-        return winner(person,n,k,person_left,ind);
+        
+        return winner(n,k)+1;
     }
 };
