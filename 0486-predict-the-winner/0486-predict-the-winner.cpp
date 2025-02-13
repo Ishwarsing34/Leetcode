be5a962f067fe4941ab89152d765abc64b6623a4
Ishwarsing34 <igirase65@gmail.com>
2025-02-13 15:51:12 +0530

    Time: 0 ms (100%), Space: 9.7 MB (52.1%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    bool helper(int start,int end, int a, int b,int flag,vector<int>&nums){
+        if(start>end) return a>=b;
+
+        bool sum;
+
+        if(flag == 1){
+            sum = helper(start+1,end,a+nums[start],b,2,nums) || helper(start,end-1,a+nums[end],b,2,nums);
+        }else{
+            sum =helper(start+1,end,a,b+nums[start],1,nums) && helper(start,end-1,a,b+nums[end],1,nums);
+        }
+
+
+        return sum;
+    }
+public:
+    bool predictTheWinner(vector<int>& nums) {
+         
+
+         return helper(0,nums.size()-1,0,0,1,nums);
+    }
+};
