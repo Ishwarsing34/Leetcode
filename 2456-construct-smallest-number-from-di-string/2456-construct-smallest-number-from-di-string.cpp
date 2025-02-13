e6964307da759ab806e6e72a99a2f197422fa0de
Ishwarsing34 <igirase65@gmail.com>
2025-02-13 16:36:21 +0530

    Time: 34 ms (8.61%), Space: 7.6 MB (99.52%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -1,38 +1,36 @@
 class Solution {
-private:
-    bool check(const string &v, const string &pattern) {
+    bool check(string &v, string &pattern) {
         for (int i = 0; i < pattern.size(); i++) {
-            if (pattern[i] == 'I' && v[i] < v[i + 1])
-                continue;
-            else if (pattern[i] == 'D' && v[i] > v[i + 1])
-                continue;
-            else
-                return false;
+            if (pattern[i] == 'I' && v[i] < v[i + 1]) continue;
+            else if (pattern[i] == 'D' && v[i] > v[i + 1]) continue;
+            else return false;
         }
         return true;
     }
 
+    string number(string &v, string &pattern, int ind) {
+        if (ind == pattern.size()) {
+            if (check(v, pattern)) {
+                return v;
+            }
+            return "";  
+        }
+
+        for (int i = ind; i < v.size(); i++) {
+            swap(v[i], v[ind]);
+            string res = number(v, pattern, ind + 1); 
+            swap(v[i], v[ind]);
+            if (!res.empty()) return res; 
+        }
+        return "";
+    }
+
 public:
     string smallestNumber(string pattern) {
-        int n = pattern.size();
-
-       
-        string v = "";
-        for (int i = 1; i <= n + 1; i++) {
-            v.push_back('0' + i); //use to convert int i into corresponding val
-            // '0' + 2 = 48 + 2 = 50  --> 50 corrsponds to 2;
+        string v = "";  
+        for (int i = 1; i <= pattern.size() + 1; i++) {
+            v.push_back('0' + i);
         }
-
-       
-        bool found = false;
-        while (!found) {
-            if (check(v, pattern)) {
-                found = true; 
-            } else {
-                next_permutation(v.begin(), v.end()); 
-            }
-        }
-
-        return v;
+        return number(v, pattern, 0);
     }
 };
