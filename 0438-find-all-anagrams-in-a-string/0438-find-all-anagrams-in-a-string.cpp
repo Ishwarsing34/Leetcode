711632913ae766cd784280294cbcd09b589bedfa
Ishwarsing34 <igirase65@gmail.com>
2025-02-23 15:34:59 +0530

    Time: 3 ms (81.25%), Space: 11.5 MB (65.34%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,44 +1,51 @@
 class Solution {
-    bool allzero(vector<int>& counter) {
+    bool allzero(vector<int>&counter){
 
-        for (auto i : counter) {
-            if (i != 0)
-                return false;
-        }
+       for(auto i : counter)
+       {
+         if(i != 0) return false;
+       }
 
         return true;
     }
-
 public:
     vector<int> findAnagrams(string s, string p) {
-        int n = s.size();
-        vector<int>counter(26, 0);
+        
+        vector<int>counter(26,0);
+        vector<int>ans;
 
-        for (int i = 0; i < p.size(); i++) {
+        int n = s.size();
+
+
+        for(int i=0;i<p.size();i++)
+        {
             counter[p[i] - 'a']++;
         }
 
-        int j = 0;
-        int i = 0;
-        vector<int> result;
-
         int k = p.size();
 
-        while (j < n) {
-            counter[s[j] - 'a']--;
+        int i = 0;
+        int j = 0;
 
-            if (j - i + 1 == k) {
-                if (allzero(counter)) {
-                    result.push_back(i);
+        while(j<n)
+        {
+            counter[s[j]-'a']--;
+
+
+            if(j-i+1 == k){
+
+                if(allzero(counter)){
+                    ans.push_back(i);
                 }
 
-                counter[s[i] - 'a']++;
+                counter[s[i]-'a']++;
                 i++;
             }
+
 
             j++;
         }
 
-        return result;
+        return ans;
     }
 };
