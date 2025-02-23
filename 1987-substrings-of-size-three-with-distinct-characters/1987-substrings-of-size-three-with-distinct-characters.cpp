3d14de95d04752a428ad109aa8c45e2531ec30d3
Ishwarsing34 <igirase65@gmail.com>
2025-02-23 15:23:15 +0530

    Time: 4 ms (14.39%), Space: 11.2 MB (6.16%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,9 +1,10 @@
 class Solution {
+    bool check(string s) {
+        set<int> st;
 
-    bool isgood(string s) {
         if (s.size() < 3)
             return false;
-        set<int> st;
+
         for (auto i : s) {
             st.insert(i);
         }
@@ -13,13 +14,12 @@ class Solution {
 
 public:
     int countGoodSubstrings(string s) {
-        if (s.size() < 3)
-            return false;
-        int n = s.size();
+
         int cnt = 0;
+        int n = s.size();
 
         for (int i = 0; i <= n - 3; i++) {
-            if (isgood(s.substr(i, 3))) {
+            if (check(s.substr(i, 3))) { 
                 cnt++;
             }
         }
