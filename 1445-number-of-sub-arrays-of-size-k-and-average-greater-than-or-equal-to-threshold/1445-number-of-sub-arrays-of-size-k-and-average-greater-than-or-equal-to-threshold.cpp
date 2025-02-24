2356cb995173d671e8bac4173f1bdbd4e6b7a74b
Ishwarsing34 <igirase65@gmail.com>
2025-02-24 10:48:44 +0530

    Time: 0 ms (100%), Space: 59.3 MB (30.61%) - LeetHub

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -1,33 +1,30 @@
 class Solution {
-    // bool checkAvg(vector<int> nums, int i, int j, int threshold, int k) {
-    //     int sum = 0;
-
-    //     for (int k = i; k <= j; k++) {
-    //         sum += nums[k];
-    //     };
-
-    //     return (sum / k >= threshold);
-    // }
-
 public:
-    int numOfSubarrays(vector<int>& nums, int k, int t) {
-        int n = nums.size();
-        int sum = 0;
-        int cnt = 0;
+    int numOfSubarrays(vector<int>& arr, int k, int t) {
+        int n = arr.size();
+
 
         int i = 0;
         int j = 0;
 
-        while (j < n) {
-            sum += nums[j];
-            while (j - i + 1 == k) {
-                if (sum / k >= t) {
-                    cnt++;
-                }
-                sum -= nums[i];
-                i++;
-            }
-            j++;
+        int sum = 0;
+        int cnt = 0;
+        while(j<n)
+        {
+           sum+=arr[j];
+
+           while(j-i+1 == k)
+           {
+              if(sum/k >= t){
+                  cnt++;
+              }
+
+              sum -= arr[i];
+
+              i++;
+           }
+
+           j++;
         }
 
         return cnt;
