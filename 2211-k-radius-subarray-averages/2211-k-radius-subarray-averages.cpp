7144f5ea3999b07ac6aa118ac341cc967305ca3a
Ishwarsing34 <igirase65@gmail.com>
2025-02-24 11:33:15 +0530

    Time: 8 ms (42.35%), Space: 134.3 MB (85.11%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -1,45 +1,48 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        //using slidng window
         int n = nums.size();
-
-        if(k==0) return nums;
 
         vector<int>result(n,-1);
 
-        if(n < 2*k+1) {
-            return result;
+        if(n < 2*k+1) return result;
 
-        }
-       long long window = 0;
+
         int left = 0;
         int right = 2*k;
+        long long window = 0;
+        int count = 2*k+1;
+
+
         int i = k;
 
-        for(int i=left;i<=right;i++){
-            window += nums[i];
+
+        for(int k=left;k<=right;k++){
+               window += nums[k];
         }
+       
+       result[i] = window/count;
+       i++;
+    //    left++;
+       right++;
 
-        int count = 2*k+1;
-        result[i] = window/count;
-        i++;
+       while(right<n){
 
-        right++;
+          int in = nums[right];
+          int out = nums[left]; 
 
-        while(right<n) {
-            int out = nums[left];
-            int in = nums[right];
+          window -= out;
+          window += in;;
 
-            window += in - out;
+          result[i] = window/count;
 
-            result[i] = window/count;
+          i++;
+          left++;
+          right++;
+       }
 
-            i++;
-            right++;
-            left++;
-        }
 
-        return result;
+       return result;
+
     }
 };
