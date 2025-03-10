59337ddc0f575c3686769003fef3700f044e18cb
Ishwarsing34 <igirase65@gmail.com>
2025-03-10 10:41:29 +0530

    Time: 155 ms (25.47%), Space: 145.8 MB (18.09%) - LeetHub

--- 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
+++ 2388-replace-elements-in-an-array/2388-replace-elements-in-an-array.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    vector<int> arrayChange(vector<int>& nums,
-                            vector<vector<int>>& operations) {
+    vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
         int n = nums.size();
-        unordered_map<int, int> mp;
 
-        for (int i = 0; i < n; i++) {
+
+        unordered_map<int,int>mp;
+
+
+        for(int i=0;i<n;i++){
             mp[nums[i]] = i;
         }
 
-        for (auto op : operations) {
 
-            int idx = mp[op[0]];
-            mp.erase(op[0]);
-            mp[op[1]] = idx;
+        for(auto op : operations){
+         
+          int idx = mp[op[0]];
+          mp.erase(op[0]);
+          mp[op[1]] = idx;
         }
 
-        vector<int> ans(n, 0);
-        for (auto it : mp) {
+
+        vector<int>ans(n,0);
+
+        for(auto it:mp){
+
             ans[it.second] = it.first;
         }
 
