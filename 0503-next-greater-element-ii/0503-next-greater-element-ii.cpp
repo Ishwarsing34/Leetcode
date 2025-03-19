d43902f7843a77994d5afac730838945170e4243
Ishwarsing34 <igirase65@gmail.com>
2025-03-19 16:42:57 +0530

    Time: 100 ms (8.13%), Space: 27.8 MB (98.89%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,13 +4,14 @@ public:
         int n = nums.size();
         vector<int>ans(n,-1);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= i + n - 1; j++) {
-                int ind = j % n;
-                if (nums[i] < nums[ind]) {
-                    ans[i] = nums[ind];
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<=2*n;j++){
+               int ind = j%n;
+
+               if(nums[i]<nums[ind]){
+                   ans[i] = nums[ind];
                     break;
-                }
+               }
             }
         }
 
