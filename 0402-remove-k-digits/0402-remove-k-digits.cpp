fc204f8e43650a47c47bf08b85432f69b06bbb5f
Ishwarsing34 <igirase65@gmail.com>
2025-03-21 17:16:18 +0530

    Time: 10 ms (9.61%), Space: 11.8 MB (19.48%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -1,55 +1,34 @@
 class Solution {
-private:
-    string reversed(string res)
-    {
-        int n = res.size();
-
-        int start = 0;
-        int end = n-1;
-
-        while(start < end)
-        {
-            swap(res[start],res[end]);
-            start++;
-            end--;
-        }
-
-        return res;
-    }
 public:
     string removeKdigits(string num, int k) {
         int n = num.size();
-        stack<char>st;
-   //keep smaller digits at the start and get rid of larger ones
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && k>0 && st.top() -'0' > num[i] - '0')
-            { 
+        string res = "";
+
+        stack<char> st;
+
+        for (int i = 0; i < n; i++) {
+
+            while (!st.empty() && k > 0 && st.top() - '0' > num[i] - '0') {
+
                 k--;
                 st.pop();
-
             }
             st.push(num[i]);
         }
 
-        while(k>0)
-        {
-            st.pop();
+        while (k > 0) {
             k--;
-        }
-
-        string res = "";
-
-        while(!st.empty())
-        {
-            res+=st.top();
             st.pop();
         }
 
-        res = reversed(res);
+        while (!st.empty()) {
+            res += st.top();
+            st.pop();
+        }
 
-        while(res.size()>0 && res[0] == '0')
-        {
+        reverse(res.begin(),res.end());
+
+        while (res.size() > 0 && res[0] == '0') {
             res.erase(res.begin());
         }
 
