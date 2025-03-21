65a42d74076ce02f17bbc8d265894a86e2cd8647
Ishwarsing34 <igirase65@gmail.com>
2025-03-21 16:19:47 +0530

    Time: 23 ms (24.67%), Space: 47.1 MB (78.45%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -4,39 +4,47 @@ public:
         int n = arr.size();
         int M = 1e9 + 7;
 
+        stack<int> st;
+
         vector<int> NSL(n, -1);
         vector<int> NSR(n, n);
 
-        stack<int> st;
-
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i]) { //>= for cosnidering duplicate elements
+
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
                 st.pop();
             }
+
             NSL[i] = st.empty() ? -1 : st.top();
+
             st.push(i);
         }
 
         while (!st.empty()) {
+
             st.pop();
         }
 
         for (int i = n - 1; i >= 0; i--) {
+
             while (!st.empty() && arr[st.top()] > arr[i]) {
+
                 st.pop();
             }
+
             NSR[i] = st.empty() ? n : st.top();
             st.push(i);
         }
 
-        long long sum = 0;
-        for (int i = 0; i < n; i++) {
-            long long left = i - NSL[i];
-            long long right = NSR[i] - i;
-            long long totalWays = (left * right) % M;
-            long long totalSum = (arr[i] * totalWays) % M;
+        int sum = 0;
 
-            sum = (sum + totalSum) % M;
+        for (int i = 0; i < n; i++) {
+
+            int left = i - NSL[i];
+            int right = NSR[i] - i;
+            long long total = ((long long)left * right) % M;
+            long long totalsum = ((long long)arr[i] * total) % M;
+            sum = (sum + totalsum) % M;
         }
 
         return sum;
