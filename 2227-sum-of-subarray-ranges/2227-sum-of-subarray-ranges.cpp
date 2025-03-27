fc4fcb9094c9e0e9e30c7f13e906b11aa1c4f919
Ishwarsing34 <igirase65@gmail.com>
2025-03-27 12:54:57 +0530

    Time: 0 ms (100%), Space: 15.6 MB (57.18%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.cpp
@@ -1,21 +1,61 @@
 class Solution {
 public:
-    long long subArrayRanges(vector<int>& nums) {
-        int n = nums.size();
-        long long sum = 0;  
+    long long subArrayRanges(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> NSL(n, -1), NSR(n, n);
+        vector<int> NGL(n, -1), NGR(n, n);
+        stack<int> st;
 
         for (int i = 0; i < n; i++) {
-            int small = nums[i];
-            int large = nums[i];
-
-            for (int j = i; j < n; j++) {
-                small = min(small, nums[j]);
-                large = max(large, nums[j]);
-
-                sum += (large - small);
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
             }
+            NSL[i] = st.empty() ? -1 : st.top();
+            st.push(i);
         }
 
-        return sum;
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            NSR[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] < arr[i]) {
+                st.pop();
+            }
+            NGL[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+
+        while (!st.empty()) st.pop();
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] <= arr[i]) {
+                st.pop();
+            }
+            NGR[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+
+        long long sumMin = 0, sumMax = 0;
+
+        for (int i = 0; i < n; i++) {
+            long long leftMin = i - NSL[i];
+            long long rightMin = NSR[i] - i;
+            sumMin += arr[i] * leftMin * rightMin;
+
+            long long leftMax = i - NGL[i];
+            long long rightMax = NGR[i] - i;
+            sumMax += arr[i] * leftMax * rightMax;
+        }
+
+        return sumMax - sumMin;
     }
 };
