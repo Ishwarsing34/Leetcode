de3161e4648d8b49afacdc1dbf6b6182e9141065
Ishwarsing34 <igirase65@gmail.com>
2025-03-27 11:55:25 +0530

    Time: 0 ms (100%), Space: 26 MB (61.14%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,32 +1,31 @@
+
 class Solution {
 public:
-    int trap(vector<int>& nums) {
-        int n = nums.size();
+    int trap(vector<int>& height) {
+        int n = height.size();
 
+        int pref[n];
+        int suf[n];
 
-        int prefMax[n];
-        int sufMax[n];
-       
-        prefMax[0] = nums[0];
+        pref[0] = height[0];
 
-        for(int i=1;i<n;i++)
-        {
-            
-            prefMax[i] = max(prefMax[i-1],nums[i]);
+        for (int i = 1; i < height.size(); i++) {
+
+            pref[i] = max(pref[i - 1], height[i]);
         }
 
-        sufMax[n-1] = nums[n-1];
+        suf[n - 1] = height[n - 1];
 
-        for(int i=n-2;i>=0;i--)
-        {
-            sufMax[i] = max(nums[i],sufMax[i+1]);
+        for (int i = n - 2; i >= 0; i--) {
+
+            suf[i] = max(suf[i + 1], height[i]);
         }
 
         int trap = 0;
 
-        for(int i=0;i<n;i++)
-        {
-            trap += min(prefMax[i],sufMax[i]) - nums[i];
+        for (int i = 0; i < n; i++) {
+
+            trap += min(pref[i], suf[i]) - height[i];
         }
 
         return trap;
