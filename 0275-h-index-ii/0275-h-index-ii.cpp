9c97ea631efa58f6878b3776e2b834138824e85f
Ishwarsing34 <igirase65@gmail.com>
2025-04-03 10:45:16 +0530

    Time: 0 ms (100%), Space: 22.7 MB (11.29%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int hIndex(vector<int>& cit) {
-        int n = cit.size();
 
+        int n = cit.size();
         int low = 0;
         int high = n-1;
 
-        while(low <= high)
-        {
-            int mid = low + (high - low)/2;
+        while(low<=high){
 
-            if( n - mid == cit[mid]) return cit[mid];
-            else if(n - mid > cit[mid]){
-                low = mid + 1;
+            int mid = low + (high-low)/2;
 
-            }else{
-                high = mid - 1;
-            }
+            if(n-mid == cit[mid]) return n-mid;
+            else if(n-mid < cit[mid]) high = mid - 1;
+            else low = mid + 1;
         }
 
-        return n - low;
+        return  n - low;
+
+
     }
 };
