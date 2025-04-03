337a473ef9ab8519309e19105b2ad51c2128104c
Ishwarsing34 <igirase65@gmail.com>
2025-04-03 11:27:38 +0530

    Time: 2 ms (2.55%), Space: 17.5 MB (52.88%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,49 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        int n = nums.size() - 1;
-        int start = 0;
-        int end = n;
+        int n = nums.size();
 
-        int first_pos = -1;
-        int last_pos = -1;
+        int first = -1;
+        int last = -1;
 
-        while(start <= end){
-            int mid = start + (end-start)/2;
+        int low = 0;
+        int high = n - 1;
 
-            if(nums[mid] == target){
-                first_pos = mid;
-                end = mid - 1;
-            }
-            else if(nums[mid] < target){
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
-            }
-        }
+        while (low <= high) {
 
+            int mid = low + (high - low) / 2;
 
-        start = 0;
-        end = n;
-
-        while(start <= end){
-            int mid = start + (end-start)/2;
-
-            if(nums[mid] == target){
-                last_pos = mid;
-                start = mid + 1;
-            }
-            else if(nums[mid] < target){
-                start = mid + 1;
-            }
-            else{
-                end = mid - 1;
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        ans.push_back(first_pos);
-        ans.push_back(last_pos);
+        low = 0;
+        high = n - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        vector<int>ans;
+
+        ans.push_back(first);
+        ans.push_back(last);
 
         return ans;
     }
