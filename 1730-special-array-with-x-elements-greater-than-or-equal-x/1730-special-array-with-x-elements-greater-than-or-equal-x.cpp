80f3b203be7e31e1c89219c3a5b4f6174a164310
Ishwarsing34 <igirase65@gmail.com>
2025-04-03 11:54:39 +0530

    Time: 0 ms (100%), Space: 11.2 MB (34.05%) - LeetHub

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/1730-special-array-with-x-elements-greater-than-or-equal-x.cpp
@@ -1,33 +1,39 @@
 class Solution {
+
     int checkGreater(vector<int>& nums, int x) {
+
         int n = nums.size();
+
         int low = 0;
         int high = n - 1;
-        
+
         while (low <= high) {
+
             int mid = low + (high - low) / 2;
+
             if (nums[mid] >= x) {
                 high = mid - 1;
             } else {
                 low = mid + 1;
             }
         }
-        
+
         return n - low;
     }
-    
+
 public:
     int specialArray(vector<int>& nums) {
+
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        
-        for (int x = 1; x <= n; ++x) {
+
+        sort(nums.begin(),nums.end());
+
+        for (int x = 1; x <= n; x++) {
             int cnt = checkGreater(nums, x);
             if (cnt == x) {
                 return x;
             }
         }
-        
         return -1;
     }
 };
