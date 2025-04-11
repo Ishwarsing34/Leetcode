a76e9bdaaafb258b3e44aecf7016bd853d01161f
Ishwarsing34 <igirase65@gmail.com>
2025-04-11 15:25:59 +0530

    Time: 0 ms (100%), Space: 21.9 MB (48.49%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -10,19 +10,21 @@
  * };
  */
 class Solution {
-    bool isBST(TreeNode* root, long min, long max) {
-        if (root == NULL) return true;
+    bool isBST(TreeNode* root, long min, long max){
 
-        if (root->val > min && root->val < max) {
-            bool left = isBST(root->left, min, root->val);
-            bool right = isBST(root->right, root->val, max);
-            return left && right;
-        } else {
+        if(root == NULL) return true;
+
+        if(root->val > min && root->val < max){
+         bool left = isBST(root->left,min,root->val);
+         bool right = isBST(root->right,root->val,max);
+         return left && right;
+        }else{
             return false;
         }
     }
+    
 public:
     bool isValidBST(TreeNode* root) {
-        return isBST(root, LONG_MIN, LONG_MAX);
+        return isBST(root,LONG_MIN,LONG_MAX);
     }
 };
