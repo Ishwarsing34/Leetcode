98ab395b8ddc9807da882464587263255bcb4ab7
Ishwarsing34 <igirase65@gmail.com>
2025-05-02 13:11:10 +0530

    Time: 0 ms (100%), Space: 18.7 MB (52.81%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,30 +4,30 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int> x, y;
+        vector<int>x,y;
 
-        // Step 1: Store the row and column indices of zeroes
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix[i][j] == 0) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j] == 0){
                     x.push_back(i);
                     y.push_back(j);
                 }
             }
         }
 
-        // Step 2: Set the entire row to zero
-        for (int i = 0; i < x.size(); i++) {
+        //setting the rows to zero
+
+        for(int i=0;i<x.size();i++){
             int ind = x[i];
-            for (int j = 0; j < m; j++) {  // ✅ Loop through columns
+            for(int j=0;j<m;j++){
                 matrix[ind][j] = 0;
             }
         }
 
-        // Step 3: Set the entire column to zero
-        for (int i = 0; i < y.size(); i++) {
+
+        for(int i=0;i<y.size();i++){
             int ind = y[i];
-            for (int j = 0; j < n; j++) {  // ✅ Loop through rows
+            for(int j=0;j<n;j++){
                 matrix[j][ind] = 0;
             }
         }
