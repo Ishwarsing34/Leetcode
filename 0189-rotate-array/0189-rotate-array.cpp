6834d365b2253612d3431926ecc30acc19af8b68
Ishwarsing34 <igirase65@gmail.com>
2025-06-07 10:04:19 +0530

    Time: 0 ms (100%), Space: 30.5 MB (9.5%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        
+        vector<int>ans(nums.size());
+
+        int n  = nums.size();
+
+        for(int i=0;i<n;i++){
+
+            ans[(i+k)%n] = nums[i];
+        }
+
+        nums = ans;
+        
+    }
+};
