60722178480b0497ab275ed4d5dd83e38f5112a9
Ishwarsing34 <igirase65@gmail.com>
2025-06-11 10:05:21 +0530

    Time: 0 ms (100%), Space: 7.7 MB (95.12%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
-        
-        int cnt = 0;
-        for(int i=1;i*i<=n;i++)
-        {
-                 cnt++;
-        }
 
-        return cnt;
+       return sqrt(n);
     }
 };
