f3fb8641f0b06d737157352ff5f8888ccbb9bb96
Ishwarsing34 <igirase65@gmail.com>
2025-06-25 21:43:22 +0530

    Time: 0 ms (100%), Space: 8.4 MB (25.1%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -4,13 +4,11 @@ public:
         int maxx = 0;
         int cnt = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(')
-                cnt++;
-            else if (s[i] == ')')
-                cnt--;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(') cnt++;
+            else if(s[i] == ')')cnt--;
 
-            maxx = max(cnt, maxx);
+            maxx = max(cnt,maxx);
         }
 
         return maxx;
