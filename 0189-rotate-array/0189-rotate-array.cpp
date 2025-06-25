6fcf003e9cf337bcbef3987d94d1cccc8b8323d6
Ishwarsing34 <igirase65@gmail.com>
2025-06-25 16:03:24 +0530

    Time: 0 ms (100%), Space: 30.3 MB (24.88%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,9 +4,14 @@ public:
     
       int n = nums.size();
 
-      k = k % n;
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
+      vector<int>ans(nums.size());
+
+
+      for(int i=0;i<n;i++)
+      {
+          ans[(i+k) % n] = nums[i];
+      }
+
+      nums = ans;
     }
 };
