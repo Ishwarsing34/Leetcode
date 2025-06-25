f5f97b79b259e7ce94a138319d04096cf8e7ba21
Ishwarsing34 <igirase65@gmail.com>
2025-06-25 16:07:18 +0530

    Time: 0 ms (100%), Space: 21.7 MB (92.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,26 +1,16 @@
-//Approach-3 (Binary Search)
-//T.C : O(nlogn)
-//S.C : O(1)
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        int l   = 0;
-        int r   = n-1;
-        int result = n;
-        
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            if(nums[mid] > mid) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
-            }
-        }
-        
-        return result;
-        
+         int n  = nums.size();
+
+         int exp_sum = n*(n+1)/2;
+
+         int sum  = 0;
+
+         for(int i=0;i<n;i++){
+            sum+=nums[i];
+         }
+
+         return exp_sum - sum;
     }
 };
