2281a56f9d2cef0e9f5ddf9e9185442408f401eb
Ishwarsing34 <igirase65@gmail.com>
2025-06-25 20:18:05 +0530

    Time: 0 ms (100%), Space: 8.9 MB (80.29%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -7,15 +7,12 @@ public:
         string res = "";
 
         for (int i = 0; i < n; i++) {
-
             if (s[i] == '(') {
-
                 cnt++;
                 if (cnt > 1) {
                     res.push_back(s[i]);
                 }
             } else {
-
                 cnt--;
                 if (cnt > 0) {
                     res.push_back(s[i]);
