6b5c304126f2daeaad50aa990dbebb165547f868
Ishwarsing34 <igirase65@gmail.com>
2025-06-26 09:30:35 +0530

    Time: 0 ms (100%), Space: 9.9 MB (68.12%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,47 +1,44 @@
 class Solution {
     bool check(vector<int>&cnt)
     {
-        for(auto i:cnt)
-        {
-            if(i!=0) return false;
+        for(auto i:cnt){
+
+            if(i != 0) return false;
         }
 
         return true;
     }
 public:
     bool checkInclusion(string s1, string s2) {
-        int k = s1.size();
-
-        int n = s2.size();
-
+        
+        int n = s1.size();
+        int m = s2.size();
 
         vector<int>cnt(26,0);
 
-
-        for(int i=0;i<k;i++)
+        for(int i=0;i<n;i++)
         {
             cnt[s1[i]-'a']++;
         }
 
-        int i = 0,j = 0;
+        int i = 0 , j = 0;
 
-
-        while(j < n)
+        while(j<m)
         {
-             cnt[s2[j]-'a']--;
+            cnt[s2[j]-'a']--;
 
-             if(j-i+1 == k)
-             {
-                if(check(cnt))
-                {
+            while(j-i+1 == n){
+
+                if(check(cnt)){
                     return true;
                 }
 
                 cnt[s2[i]-'a']++;
                 i++;
-             }
+            }
 
-             j++;
+            j++;
+
         }
 
         return false;
