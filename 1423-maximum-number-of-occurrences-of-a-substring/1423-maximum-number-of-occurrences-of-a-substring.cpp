c129505f335a5de7fff627acbd3681126bed5db2
Ishwarsing34 <igirase65@gmail.com>
2025-06-26 11:21:53 +0530

    Time: 299 ms (18.57%), Space: 83.2 MB (31%) - LeetHub

--- 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.cpp
+++ 1423-maximum-number-of-occurrences-of-a-substring/1423-maximum-number-of-occurrences-of-a-substring.cpp
@@ -1,33 +1,35 @@
 class Solution {
-    bool isValid(string& cur, int maxLet) {
-        map<char, int> mpp;
+    bool isValid(string &s,int maxLet)
+    {
+        map<char,int>mpp;
 
-        for (auto it : cur) {
-            mpp[it]++;
+        for(auto i : s)
+        {
+            mpp[i]++;
         }
 
         return mpp.size() <= maxLet;
     }
-
 public:
-    int maxFreq(string s, int maxLet, int minSize, int maxSize) {
+    int maxFreq(string s, int maxLet, int minsize, int maxsize) {
+        
+
         int n = s.size();
-        if (n == 0 || maxLet == 0)
-            return 0;
+        map<string,int>mp;
+        int maxi = 0;
 
-        map<string, int> mp;
+        for(int i=0;i<=n-minsize;i++)
+        {
+            string curr = s.substr(i,minsize);
 
-        int maxx = 0;
-        for (int i = 0; i <= n - minSize; i++) {
-
-            string cur = s.substr(i, minSize);
-
-            if (isValid(cur, maxLet)) {
-                mp[cur]++;
-                maxx = max(maxx, mp[cur]);
+            if(isValid(curr,maxLet))
+            {
+                mp[curr]++;
+                maxi = max(maxi,mp[curr]);
             }
         }
 
-        return maxx;
+        return maxi;
     }
+
 };
