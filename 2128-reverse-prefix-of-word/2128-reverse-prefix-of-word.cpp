2a885234ae6da85e723deac184e1b4f7b78db1dd
Ishwarsing34 <igirase65@gmail.com>
2025-06-26 13:12:40 +0530

    Time: 0 ms (100%), Space: 8.3 MB (70.32%) - LeetHub

--- 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     string reversePrefix(string word, char ch) {
-        int n = word.size();
 
-        string ans = "";
+        int n = word.size();
         int ind = -1;
+        string ans = "";
 
         for (int i = 0; i < n; i++) {
+
             ans.push_back(word[i]);
 
-            if (word[i] == ch) {
+            if (word[i] == ch)
+
+            {
                 ind = i;
                 reverse(ans.begin(), ans.end());
                 break;
             }
         }
-        if(ind != -1){
-        for (int i = ind + 1; i < n; i++) {
 
-            ans.push_back(word[i]);
+        if (ind != -1) {
+
+            for (int i = ind + 1; i < n; i++) {
+                ans.push_back(word[i]);
+            }
         }
 
-
-        }
-        
         return ans;
     }
 };
