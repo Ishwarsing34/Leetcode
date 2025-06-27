6bcbbfc3dbd9dace251a14675b063d2c312817a0
Ishwarsing34 <igirase65@gmail.com>
2025-06-27 10:15:42 +0530

    Time: 11 ms (88.68%), Space: 47.1 MB (61%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,51 +1,57 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
-        int M = 1e9 + 7;
+        
         int n = arr.size();
+        int M = 1e9 + 7;
 
-        vector<int> NSL(n, -1);
-        vector<int> NSR(n, n);
+        stack<int>st;
 
-        stack<int> st;
+        vector<int>NSR(n,n);
+        vector<int>NSL(n,-1);
 
-        for (int i = 0; i < n; i++) {
 
-            while (!st.empty() && arr[st.top()] >= arr[i]) {
-
-                st.pop();
-            }
-
-            NSL[i] = st.empty() ? -1 : st.top();
-
-            st.push(i);
-        }
-
-        while (!st.empty()) {
-
-            st.pop();
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!st.empty() && arr[st.top()] > arr[i]) {
+        for(int i=n-1;i>=0;i--)
+        {
+            while(!st.empty() && arr[st.top()] > arr[i])
+            {
                 st.pop();
             }
 
             NSR[i] = st.empty() ? n : st.top();
-
             st.push(i);
         }
 
-        int sum = 0;
 
-        for (int i = 0; i < n; i++) {
+        while(!st.empty())
+        {
+            st.pop();
+        }
 
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && arr[st.top()] >= arr[i])
+            {
+                st.pop();
+            }
+
+             NSL[i] = st.empty() ? -1 : st.top();
+             st.push(i);
+        }
+     
+
+       int sum = 0;
+
+        for(int i=0;i<n;i++)
+        {
             int left = i - NSL[i];
             int right = NSR[i] - i;
-            long long totalsum = ((long long)left * right) % M;
-            long long total = ((long long)arr[i] * totalsum) % M;
-            sum = (sum + total) % M;
+
+            long long totalsum = ((long long )left * right)%M ;
+            long long total = ((long long)arr[i] * totalsum)%M;
+
+            sum = (sum + total)%M;
+
         }
 
         return sum;
