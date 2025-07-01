7a95f74954a4ede52d3bc436782d62c2ffb97797
Ishwarsing34 <igirase65@gmail.com>
2025-07-01 17:38:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,73 +1,63 @@
 class Solution {
 
-    int findMin(vector<int>& nums,int low, int high) {
-
+    int findMin(vector<int>&nums)
+    {
         int n = nums.size();
 
-        
+        int low = 0 , high = n-1;
 
-        while (low < high) {
+        while(low < high)
+        {
+           
 
-            while (low < high && nums[low] == nums[low + 1]) {
-                low++;
-            }
+            while(low < high && nums[low] == nums[low+1]) low++;
+            while(low < high && nums[high] == nums[high-1]) high--;
+             int mid = low + (high-low)/2;
 
-            while (low < high && nums[high] == nums[high - 1]) {
-                high--;
-            }
-
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] > nums[high]) {
-                low = mid + 1;
-            } else {
+            if(nums[mid] > nums[high]){
+                low = mid+1;
+            }else{
                 high = mid;
             }
         }
-
         return high;
     }
 
-    int BinarySearch(int l, int r, vector<int>& nums, int target) {
+    int search(int l, int r , vector<int>&nums,int target)
+    {
+        int low = l;
+        int high = r;
+        int idx = -1;
 
-        int ind = -1;
+        while(low<=high)
+        {
+            int mid = low+(high-low)/2;
 
-        while (l <= r) {
-
-            int mid = l + (r - l) / 2;
-
-            if (nums[mid] == target) {
-                ind = mid;
+            if(nums[mid] == target){
+                idx = mid;
                 break;
-            } else if (nums[mid] > target) {
-                r = mid - 1;
-            }
-
-            else {
-                l = mid + 1;
+            }else if(nums[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
-
-        return ind;
+        return idx;
     }
-
 public:
+
     bool search(vector<int>& nums, int target) {
+        
 
+        int pivot = findMin(nums);
+        
         int n = nums.size();
-
-        int low = 0;
-        int high = n - 1;
 
         int idx = -1;
 
-        int pivotIdx = findMin(nums, low, high);
+        idx = search(0,pivot-1,nums,target);
 
-        idx = BinarySearch(0, pivotIdx - 1, nums, target);
-
-        if(idx != -1) return true;
-
-        idx = BinarySearch(pivotIdx, n - 1, nums, target);
+        idx = search(pivot,n-1,nums,target);
 
         return idx == -1 ? false : true;
     }
