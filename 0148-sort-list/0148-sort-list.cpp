a66c020796dd6b268027aba1d8083886a8aa525a
Ishwarsing34 <igirase65@gmail.com>
2025-07-01 10:38:06 +0530

    Time: 48 ms (38.57%), Space: 75.8 MB (8.81%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,30 +10,57 @@
  */
 class Solution {
 public:
+    ListNode* findMid(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+
+    ListNode* merge2(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while (list1 != NULL && list2 != NULL) {
+            if (list1->val < list2->val) {
+                temp->next = list1;
+                temp = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                temp = list2;
+                list2 = list2->next;
+            }
+
+            if (list1 != NULL)
+                temp->next = list1;
+            else
+                temp->next = list2;
+        }
+
+        return dummy->next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int>ans;
-        ListNode* temp = head;
 
-        while(temp!=NULL)
-        {
-            ans.push_back(temp->val);
-            temp = temp->next;
+        if (head == NULL || head->next == NULL)
+            return head;
 
-        }
+        ListNode* mid = findMid(head);
+        ListNode* right = mid->next;
+        mid->next = nullptr;
 
-        sort(ans.begin(),ans.end());
+        ListNode* left = sortList(head);
+        right = sortList(right);
 
-        int i=0;
-        temp = head;
-
-        while(temp!=NULL)
-        {
-            temp->val=ans[i];
-            i++;
-            temp=temp->next;
-        }
-
-        return head;
-    
+        return merge2(left, right);
     }
 };
