96b0ca5011809fcaa73174097296953088d9e9a1
Ishwarsing34 <igirase65@gmail.com>
2025-07-01 17:27:02 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.26%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,14 +1,9 @@
 class Solution {
-
-    int findMin(vector<int>& nums) {
-
+    int findmin(vector<int>& nums) {
         int n = nums.size();
-
-        int low = 0;
-        int high = n - 1;
+        int low = 0, high = n - 1;
 
         while (low < high) {
-
             int mid = low + (high - low) / 2;
 
             if (nums[mid] > nums[high]) {
@@ -17,28 +12,25 @@ class Solution {
                 high = mid;
             }
         }
+
         return high;
     }
 
-    int BinarySearch(int l, int r, vector<int>& nums, int target) {
-
+    int binarysearch(int l, int r, vector<int>& nums, int target) {
         int low = l;
         int high = r;
-
         int ind = -1;
 
         while (low <= high) {
-
             int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
                 ind = mid;
                 break;
-            } else if (nums[mid] < target) {
+            } else if (nums[mid] < target)
                 low = mid + 1;
-            } else {
+            else
                 high = mid - 1;
-            }
         }
 
         return ind;
@@ -47,18 +39,16 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int pivotInd = findMin(nums);
-
         int n = nums.size();
 
-        int idx = -1;
+        int pivotInd = findmin(nums);
 
-        idx = BinarySearch(0, pivotInd - 1, nums, target);
+        int idx = binarysearch(0, pivotInd - 1, nums, target);
 
         if (idx != -1)
             return idx;
 
-        idx = BinarySearch(pivotInd, n - 1, nums, target);
+        idx = binarysearch(pivotInd, n - 1, nums, target);
 
         return idx;
     }
