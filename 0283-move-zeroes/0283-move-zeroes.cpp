f518973ac2819e753ed4824d245b719710a2bef8
Ishwarsing34 <igirase65@gmail.com>
2025-08-20 20:25:04 +0530

    Time: 0 ms (100%), Space: 23.9 MB (55.03%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -4,14 +4,16 @@ public:
         
         int n = nums.size();
 
-        int j  = 0;
+        int j = 0;
 
         for(int i=0;i<n;i++)
         {
             if(nums[i] != 0){
-                swap(nums[i],nums[j]);
-                j++;
+               swap(nums[i],nums[j]);
+               j++;
             }
         }
+
+        
     }
 };
