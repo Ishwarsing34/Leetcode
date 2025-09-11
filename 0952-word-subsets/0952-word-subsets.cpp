5f563d71810ec29f50defb3880596a4d1aa69a8f
Ishwarsing34 <igirase65@gmail.com>
2025-09-11 10:43:50 +0530

    Time: 60 ms (46.54%), Space: 106.7 MB (74.42%) - LeetHub

--- 0952-word-subsets/0952-word-subsets.cpp
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        vector<int> maxFreq(26, 0);
 
-        // Build the maximum required frequency from words2
-        for (string &w : words2) {
+        vector<int> maxfreq(26, 0);
+
+        vector<string> ans;
+        for (string& w : words2) {
             vector<int> freq(26, 0);
-            for (char c : w) freq[c - 'a']++;
+
+                for (auto c : w) {
+                freq[c - 'a']++;
+            }
+
             for (int i = 0; i < 26; i++) {
-                maxFreq[i] = max(maxFreq[i], freq[i]);
+                maxfreq[i] = max(maxfreq[i], freq[i]);
             }
         }
 
-        vector<string> ans;
-        // Check each word in words1
-        for (string &w : words1) {
+        for (string& w : words1) {
+
             vector<int> freq(26, 0);
-            for (char c : w) freq[c - 'a']++;
+
+            for (char c : w) {
+                freq[c - 'a']++;
+            }
 
             bool ok = true;
+
             for (int i = 0; i < 26; i++) {
-                if (freq[i] < maxFreq[i]) {
+                if (freq[i] < maxfreq[i]) {
                     ok = false;
                     break;
                 }
             }
-            if (ok) ans.push_back(w);
+
+            if ((ok))
+                ans.push_back(w);
         }
 
         return ans;
