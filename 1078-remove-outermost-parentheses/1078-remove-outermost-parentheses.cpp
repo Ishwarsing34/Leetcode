d814e7a4bfd70e4a2539a54cf981b118dddcb45a
Ishwarsing34 <igirase65@gmail.com>
2025-09-17 17:57:51 +0530

    Time: 2 ms (22.34%), Space: 8.8 MB (80.4%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+
         int n = s.size();
 
-        int cnt = 0;
         string res = "";
+
+        int cnt = 0;
 
         for (int i = 0; i < n; i++) {
             if (s[i] == '(') {
                 cnt++;
-                if (cnt > 1) {
+                if (cnt > 1)
                     res.push_back(s[i]);
-                }
             } else {
                 cnt--;
-                if (cnt > 0) {
+                if (cnt > 0)
                     res.push_back(s[i]);
-                }
             }
         }
 
