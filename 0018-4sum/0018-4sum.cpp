81aa752e4ba11261bdb0061a1a3093780b2ef346
Ishwarsing34 <igirase65@gmail.com>
2025-11-23 19:19:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        
-        vector<vector<int>>ans;
-
-        sort(nums.begin(),nums.end());
 
         int n = nums.size();
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++){
+        vector < vector<int>> ans;
 
-                int k = j+1 , l = n -1;
+        sort(nums.begin(), nums.end());
 
-                while(k < l)
-                {
-                    long long sum = 1LL * nums[i] + nums[j] +  nums[k] + nums[l];
+        for (int i = 0; i < n-3; i++) {
+            for (int j = i + 1; j < n-2; j++) {
 
-                    if(sum == target){
+                int k = j + 1, l = n - 1;
 
-                        ans.push_back({nums[i],nums[j],nums[k],nums[l]});
+                while (k < l) {
+                    long long sum = 1LL * nums[i] + nums[j] + nums[k] + nums[l];
 
+                    if (sum == target) {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
 
-                        while(k < l && nums[k] == nums[k+1]) k++;
-                        while( k < l && nums[l] == nums[l-1]) l--;
+                        while (k < l && nums[k] == nums[k + 1])
+                            k++;
 
-                        k++,l--;
-                    }else if(sum < target) k++;
-                    else l--;
+                        while (k < l && nums[l] == nums[l - 1])
+                            l++;
 
+                        k++, l--;
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
+                    }
                 }
 
-                while(j+1<n && nums[j] == nums[j+1]) j++;
-
+                while (j + 1 < n && nums[j] == nums[j + 1])
+                    j++;
             }
 
-            while(i+1<n && nums[i] == nums[i+1]) i++;
+            while (i + 1 < n && nums[i] == nums[i + 1])
+                i++;
         }
+
 
         return ans;
     }
