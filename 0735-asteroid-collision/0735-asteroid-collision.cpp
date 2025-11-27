cdbdd9bb0cc9f2902e455e429e6b91e0d33819fc
Ishwarsing34 <igirase65@gmail.com>
2025-11-28 05:16:30 +0530

    Time: 0 ms (100%), Space: 22.1 MB (39.39%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -12,10 +12,10 @@ public:
 
                 int sum = st.top() + ast[i];
 
-                if (sum < 0) {
-                    st.pop();
-                } else if (sum > 0) {
+                if (sum > 0) {
                     ast[i] = 0;
+                } else if (sum < 0) {
+                    st.pop();
                 } else {
                     st.pop();
                     ast[i] = 0;
