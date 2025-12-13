0484e899a9e22524e8542be42a8e10df938d307c
Ishwarsing34 <igirase65@gmail.com>
2025-12-13 23:16:43 +0530

    Time: 6 ms (8.05%) | Memory: 14.1 MB (23.69%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -1,37 +1,33 @@
 class Solution {
     vector<int> solve(string s) {
+
         vector<int> res;
 
         for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
 
-            if (ch == '-' || ch == '+' || ch == '*') {
+            if (s[i] == '-' || s[i] == '+' || s[i] == '*') {
+
                 vector<int> left = solve(s.substr(0, i));
-
                 vector<int> right = solve(s.substr(i + 1));
 
                 for (int& x : left) {
-                    for (int& y : right) {
+                    for (int &y : right) {
 
-                        if (ch == '+') {
-                            res.push_back(x + y);
-                        } else if (ch == '-') {
+                        if (s[i] == '-')
                             res.push_back(x - y);
-                        } else {
+                        else if (s[i] == '*')
                             res.push_back(x * y);
-                        }
+                        else
+                            res.push_back(x + y);
                     }
                 }
             }
-
         }
 
+        if (res.empty()) {
+            res.push_back(stoi(s));
+        }
 
-        
-            if (res.empty()) {
-
-                res.push_back(stoi(s));
-            }
 
         return res;
     }
