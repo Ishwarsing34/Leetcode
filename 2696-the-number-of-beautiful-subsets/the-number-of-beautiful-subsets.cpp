04333a172c844f29efb269dc6ed33a1c05625b45
Ishwarsing34 <igirase65@gmail.com>
2026-01-14 06:04:56 +0530

    Time: 167 ms (62.53%) | Memory: 39.2 MB (56.08%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -5,18 +5,18 @@ class Solution {
                unordered_map<int, int>& mp) {
 
         if (ind == nums.size()) {
-            count++;   // counts one valid subset (possibly empty)
+            count++;   
             return;
         }
 
-        // 🔹 Option 1: NOT pick nums[ind]
+       
         solve(nums, ind + 1, k, mp);
 
-        // 🔹 Option 2: pick nums[ind] (only if valid)
+        
         if (mp[nums[ind] - k] == 0 && mp[nums[ind] + k] == 0) {
             mp[nums[ind]]++;
             solve(nums, ind + 1, k, mp);
-            mp[nums[ind]]--;   // backtrack
+            mp[nums[ind]]--;  
         }
     }
 
@@ -26,6 +26,6 @@ public:
 
         solve(nums, 0, k, mp);
 
-        return count - 1;  // remove empty subset
+        return count - 1;  
     }
 };
